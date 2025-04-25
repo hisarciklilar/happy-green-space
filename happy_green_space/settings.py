b6a8bba8c17bd4ccc387cdbfd1649cad2384b61c
hisarciklilar <hisarciklilar@gmail.com>
2025-04-25 11:20:51 +0100

    Prepare for deployment

--- happy_green_space/settings.py
+++ happy_green_space/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r*yn2@#hf*b9dh655h@qm6jp90m5oxj2jq7+%s0lnszc8)r&l+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost',
+                 '.herokuapp.com',
+                 '127.0.0.1:8000',]
 
 # Application definition
 
