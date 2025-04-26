8eef8761beb6439c900b3b9df03a8d0e639cafa2
hisarciklilar <hisarciklilar@gmail.com>
2025-04-26 10:53:44 +0100

    Add landing app

--- happy_green_space/settings.py
+++ happy_green_space/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'garden_gossip',  # app for gardeners' forum
+    'landing',  # app for landing page
 ]
 
 MIDDLEWARE = [
--- happy_green_space/urls.py
+++ happy_green_space/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from garden_gossip.views import gossip_corner
+from landing.views import index
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('gossip_corner/', gossip_corner, name='gossip_corner'),
+    path('index/', index, name='index'),
 ]
