cabbc01a2fb063e78d0d57e13a03f457354b1731
Mehtap Hisarciklilar <68424810+hisarciklilar@users.noreply.github.com>
2025-08-01 19:42:08 +0300

    Add Forum Model

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, 
+                               on_delete=models.CASCADE, related_name='forum_posts')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+class Reply(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, 
+                             related_name='replies')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    
