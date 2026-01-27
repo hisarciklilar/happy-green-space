fe93fae0f54adfff5f1ac0e63c2b83108da743e8
hisarciklilar <hisarciklilar@gmail.com>
2026-01-27 10:29:17 +0000

    Add url accessible & url resolves tests

--- main/tests.py
+++ main/tests.py
@@ -15,6 +15,7 @@ class MainPagesStatusCodeTests(TestCase):
         response = self.client.get(reverse("main:tasks_january"))
         self.assertEqual(response.status_code, 200)
 
+
 class MainTemplateUsedTests(TestCase):
     def test_home_page_uses_correct_template(self):
         response = self.client.get(reverse("main:home"))
@@ -27,3 +28,31 @@ class MainTemplateUsedTests(TestCase):
     def test_tasks_january_page_uses_correct_template(self):
         response = self.client.get(reverse("main:tasks_january"))
         self.assertTemplateUsed(response, "main/tasks_january.html")
+
+
+class MainUrlAccessibleTests(TestCase):
+    def test_home_page_url(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_url(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_tasks_january_page_url(self):
+        response = self.client.get("/tasks_january/")
+        self.assertEqual(response.status_code, 200)
+
+
+class MainUrlResolvesTests(TestCase):
+    def test_home_page_resolves(self):
+        url = reverse("main:home")
+        self.assertEqual(url, "/")
+
+    def test_about_page_resolves(self):
+        url = reverse("main:about")
+        self.assertEqual(url, "/about/")
+
+    def test_tasks_january_page_resolves(self):
+        url = reverse("main:tasks_january")
+        self.assertEqual(url, "/tasks_january/")
