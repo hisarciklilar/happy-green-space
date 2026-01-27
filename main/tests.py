b753152a5641ada9f9dedc1e759621929c026836
hisarciklilar <hisarciklilar@gmail.com>
2026-01-27 10:16:59 +0000

    Add template used tests to main pages

--- main/tests.py
+++ main/tests.py
@@ -14,3 +14,16 @@ class MainPagesStatusCodeTests(TestCase):
     def test_tasks_january_page_returns_200(self):
         response = self.client.get(reverse("main:tasks_january"))
         self.assertEqual(response.status_code, 200)
+
+class MainTemplateUsedTests(TestCase):
+    def test_home_page_uses_correct_template(self):
+        response = self.client.get(reverse("main:home"))
+        self.assertTemplateUsed(response, "main/home.html")
+
+    def test_about_page_uses_correct_template(self):
+        response = self.client.get(reverse("main:about"))
+        self.assertTemplateUsed(response, "main/about.html")
+
+    def test_tasks_january_page_uses_correct_template(self):
+        response = self.client.get(reverse("main:tasks_january"))
+        self.assertTemplateUsed(response, "main/tasks_january.html")
