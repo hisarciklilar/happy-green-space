b5160ddfa78e8f45d1ce2f4a1583ddc42f41d345
hisarciklilar <hisarciklilar@gmail.com>
2026-01-27 13:49:03 +0000

    Add post list vies status code and template tests

--- forum/tests.py
+++ forum/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class PostListViewTests(TestCase):
+    def test_post_list_view_status_code(self):
+        response = self.client.get(reverse("forum:post_list"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_list_view_template_used(self):
+        response = self.client.get(reverse("forum:post_list"))
+        self.assertTemplateUsed(response, "forum/post_list.html")
